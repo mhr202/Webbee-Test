671fb9fb7cc725d3fe7a5c2b7ed6218f4b23b7cc
Hasnat Raza <hasnatraza202@gmil.com>
2023-05-29 10:05:11 -0400

    feat: Cleaned up some code

--- app/Validations/BookingValidation.php
+++ app/Validations/BookingValidation.php
@@ -11,6 +11,7 @@ class BookingValidation
 {
     public static function validateBooking(array $data)
     {
+        // echo ("dkfdjh");
         $validator = Validator::make($data, [
             'bookings.*.start_time' => 'required|date_format:H:i',
             'bookings.*.end_time' => 'required|date_format:H:i',
@@ -22,42 +23,9 @@ class BookingValidation
         ]);
 
         if ($validator->fails()) {
+            echo ("dkfdjh");
             return response()->json(['errors' => $validator->errors()], 400);
         }
-
-
-
-        // $validator->after(function ($validator) use ($data) {
-        //     $service = Service::find($data['service_id']);
-        //     $startTime = Carbon::createFromFormat('H:i', $data['start_time']);
-        //     $endTime = Carbon::createFromFormat('H:i', $data['end_time']);
-        //     $bookingDay = strtolower($data['day']);
-
-        //     if (!$service->isSlotAvailable($startTime, $endTime, $bookingDay)) {
-        //         $validator->errors()->add('slot_id', 'Requested slot is not available.');
-        //     }
-
-        //     if ($service->isSlotBookedOut($startTime, $endTime, $bookingDay)) {
-        //         $validator->errors()->add('slot_id', 'Requested slot is booked out.');
-        //     }
-
-        //     if ($service->isSlotInBreak($startTime, $endTime, $bookingDay)) {
-        //         $validator->errors()->add('slot_id', 'Requested slot falls between configured breaks.');
-        //     }
-
-        //     if ($service->isSlotInBreakBetweenAppointments($startTime, $endTime, $bookingDay)) {
-        //         $validator->errors()->add('slot_id', 'Requested slot falls between configured break between appointments.');
-        //     }
-
-        //     if ($service->isPlannedOffDate($data['booking_time'], $startTime, $endTime)) {
-        //         $validator->errors()->add('slot_id', 'Requested slot falls on a planned off date and time duration.');
-        //     }
-
-        //     $slot = Slot::find($data['slot_id']);
-        //     if ($slot && !$slot->isAvailable()) {
-        //         $validator->errors()->add('slot_id', 'Requested slot is not available.');
-        //     }
-        // });
 
         return null;
     }
